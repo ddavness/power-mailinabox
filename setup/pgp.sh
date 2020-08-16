1b232f14bc8bd5684d8e81e2186261b09fb8378c
David Duque <david@davness.dev>
2020-08-16 23:42:59 +0100

    Keep daemon's key fingerprint in configuration; Replace if needed

--- setup/pgp.sh
+++ setup/pgp.sh
@@ -8,13 +8,13 @@
 
 source setup/functions.sh # load our functions
 source /etc/mailinabox.conf # load global vars
-export GNUPGHOME # Dump into the environment
+export GNUPGHOME # Dump into the environment so that gpg uses it as homedir
 
 # Install gnupg
 apt_install gnupg
 
-if [ "$(gpg --list-secret-keys 2> /dev/null)" == "" ]; then
-    echo "Generating daemon's PGP keypair..."
+if [ "$(gpg --list-secret-keys 2> /dev/null)" == "" -o "${PGPKEY-}" == "" ]; then
+    echo "No keypair found. Generating daemon's PGP keypair..."
     gpg --generate-key --batch << EOF;
     %no-protection
     Key-Type: RSA
@@ -26,4 +26,7 @@ if [ "$(gpg --list-secret-keys 2> /dev/null)" == "" ]; then
     %commit
 EOF
     chown -R root:root $GNUPGHOME
+    # Remove the old key fingerprint if it exists, and add the new one
+    echo "$(cat /etc/mailinabox.conf | grep -v "PGPKEY")" > /etc/mailinabox.conf
+    echo "PGPKEY=$(gpg --list-secret-keys --with-colons | grep fpr | sed 's/fpr//g' | sed 's/://g')" >> /etc/mailinabox.conf
 fi
