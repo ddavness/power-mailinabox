152d7c39bc3c4e108b7e2269598c2d44c07f923d
David Duque <david@duqued.net>
2022-01-22 18:53:49 +0000

    Band-aid fix for B2 backups on Debian 10

--- setup/management.sh
+++ setup/management.sh
@@ -57,8 +57,8 @@ hide_output $venv/bin/pip install --upgrade \
 case $(get_os_code) in
 
 	$OS_DEBIAN_10)
-		apt_install python-pip
-		hide_output pip2 install --upgrade "b2<2.0.0"
+		apt_install python-pip python-backports.functools-lru-cache
+		hide_output pip2 install --upgrade "b2<2.0.0" "logfury<1.0.0"
 		hide_output $venv/bin/pip install --upgrade "b2<2.0.0"
 		;;
 	
