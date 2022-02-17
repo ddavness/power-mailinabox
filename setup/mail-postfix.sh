56b0fc02da2c9b40ce1a55c99a1ea9b15eece3ab
David Duque <david@duqued.net>
2022-02-17 23:25:01 +0000

    Install libsasl2-modules (#44)
    Required for SMTP relays to work

--- setup/mail-postfix.sh
+++ setup/mail-postfix.sh
@@ -42,7 +42,7 @@ source /etc/mailinabox.conf # load global vars
 # * `ca-certificates`: A trust store used to squelch postfix warnings about
 #   untrusted opportunistically-encrypted connections.
 echo "Installing Postfix (SMTP server)..."
-apt_install postfix postfix-sqlite postfix-pcre postgrey ca-certificates
+apt_install postfix postfix-sqlite postfix-pcre postgrey ca-certificates libsasl2-modules
 
 # ### Basic Settings
 
