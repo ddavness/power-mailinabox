1cc205446f5339e6c8edbbaf79fef90136941ee3
Richard Case <richard@casesolved.co.uk>
2022-06-15 05:21:42 +0100

    fix: fixed export, extra command switches and improvements

--- management/imapimport.py
+++ management/imapimport.py
@@ -8,18 +8,18 @@ import shutil
 import subprocess
 
 
-def run_cmd(cmd, config):
+def run_cmd(cmd, config, info):
 	from decimal import Decimal, getcontext
 	getcontext().prec = 3
 
 	args = ['su', 'mail', '-g', 'mail', '-s', '/bin/bash', '-c']
 	exe = f'/usr/local/lib/mailinabox/env/bin/offlineimap -c {config} '
-	if cmd in ('import', 'export'):
-		exe += '-o'
-	else:
+	if info:
 		exe += '--info'
-	args.append(exe)
+	else:
+		exe += '-o'
 
+	args.append(exe)
 	print('')
 	output = ''
 
@@ -35,7 +35,7 @@ def run_cmd(cmd, config):
 			match = regex.search(text)
 			if match:
 				percent = ( Decimal(match.group(1)) / Decimal(match.group(2)) ) * 100
-				print(f'\r{percent}%   ', end='')
+				print(f'\r{percent}% ', end='')
 			else:
 				output += text
 
@@ -48,18 +48,16 @@ def run_cmd(cmd, config):
 	print('\rDone!\n')
 	return output
 
-def importexport_cmdline(exec, cmd, type, user, remoteuser, remotepass, remotehost, remoteport, *folderlist):
+def importexport_cmdline(exec, cmd, type, user, remoteuser, remotepass, remotehost, remoteport, *folderlist, **options):
 	try:
 		remoteport = int(remoteport)
 		user, sep, domain = user.rpartition('@')
-		if cmd not in ('import', 'export', 'info') or type not in ('Gmail', 'IMAP') or not user:
-			raise SyntaxError('Error in cmd or type or user parameters')
+		if cmd not in ('import', 'export') or type not in ('Gmail', 'IMAP') or not user:
+			raise SyntaxError('Error in cmd, type or user parameters')
 
 		localtype = 'Maildir'
-		if cmd in ('import', 'info') and type == 'Gmail':
+		if type == 'Gmail' and cmd == 'import':
 			localtype = 'GmailMaildir'
-			if not folderlist:
-				folderlist = ('[Gmail]/All Mail',)
 
 	except Exception as e:
 		print(usage)
@@ -73,7 +71,7 @@ def importexport_cmdline(exec, cmd, type, user, remoteuser, remotepass, remoteho
 	# Create the config from template
 	mailroot = os.path.join(env['STORAGE_ROOT'], 'mail', 'mailboxes')
 	config = os.path.join(mailroot, 'offlineimaprc')
-	if cmd in ('import', 'info'):
+	if cmd == 'import':
 		template = os.path.join(mailroot, 'offlineimap-in.conf')
 	else:
 		template = os.path.join(mailroot, 'offlineimap-out.conf')
@@ -87,28 +85,48 @@ def importexport_cmdline(exec, cmd, type, user, remoteuser, remotepass, remoteho
 	shutil.chown(config, 'mail', 'mail')
 
 	# Run
-	output = run_cmd(cmd, config)
-	print(output)
+	output = run_cmd(cmd, config, options.get('info'))
+
+	if not options.get('quiet'):
+		print(output)
 
 
 usage = """
 Mail in a box Gmail/IMAP import/export tool
-imapimport.py cmd type user remoteuser remotepass remoteserver remoteport [folders]
-cmd		import/export/info
-type		Gmail/IMAP
+imapimport.py [options] cmd type user remoteuser remotepass remoteserver remoteport [folders]
+Options:
+-i		remote and local folder information
+-q		quiet, don't print the output from the transfer. Progress is always printed
+
+Parameters:
+cmd		import or export
+type		Gmail or IMAP
 user		local mail username, usually user@domain.tld
 remoteuser	remote server username, usually user@domain.tld
 remotepass	remote server password
 remotehost	remote server DNS name
 remoteport	remote server port number, e.g. 993
-[folders]	space separated list of source folders. If omitted for Gmail imports
-		only [Gmail]/All Mail will be imported, otherwise all folders will be synced
+[folders]	space separated list of remote (Gmail/IMAP) folders. If omitted for Gmail,
+		only '[Gmail]/All Mail' will be migrated, otherwise all folders will be allowed
+
+*** Important Note: Once an import has started you will need to make the folder visible
+                    in webmail settings.
 """
 
 if __name__ == "__main__":
 	# Commandline tool
+	args = []
+	options = {}
+	for arg in sys.argv:
+		if arg == '-i':
+			options['info'] = True
+		elif arg == '-q':
+			options['quiet'] = True
+		else:
+			args.append(arg)
+
 	try:
-		importexport_cmdline(*sys.argv)
+		importexport_cmdline(*args, **options)
 	except Exception:
 		print(usage)
 		raise
