bbaae766154caf83fa5eb06c8938dc0fac9153e5
Richard C <richard@casesolved.co.uk>
2022-06-18 00:30:08 +0100

    fix: also update the ios profile HTTPS port

--- setup/web.sh
+++ setup/web.sh
@@ -113,6 +113,7 @@ mkdir -p /var/lib/mailinabox
 chmod a+rx /var/lib/mailinabox
 cat conf/ios-profile.xml \
 	| sed "s/PRIMARY_HOSTNAME/$PRIMARY_HOSTNAME/" \
+	| sed "s/HTTPS_PORT/$HTTPS_PORT/" \
 	| sed "s/UUID1/$(cat /proc/sys/kernel/random/uuid)/" \
 	| sed "s/UUID2/$(cat /proc/sys/kernel/random/uuid)/" \
 	| sed "s/UUID3/$(cat /proc/sys/kernel/random/uuid)/" \
