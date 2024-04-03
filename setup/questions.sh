d1ff6bff212241706f32f8ddcb6e69f20457e82c
David Duque <david@duqued.net>
2024-04-03 15:09:07 +0100

    (default venv) Try installing python packages via apt before attempting to use pip

--- setup/questions.sh
+++ setup/questions.sh
@@ -14,8 +14,10 @@ if [ -z "${NONINTERACTIVE:-}" ]; then
 
 	# Installing email_validator is repeated in setup/management.sh, but in setup/management.sh
 	# we install it inside a virtualenv. In this script, we don't have the virtualenv yet
-	# so we install the python package globally.
-	hide_output pip3 install "email_validator>=1.0.0" || exit 1
+	# so we install the python package globally
+
+	# On Debian 12, this package must be installed via apt-get. Try that avenue first.
+	apt_get_quiet install python3-email-validator || hide_output pip3 install "email_validator>=1.0.0" || exit 1
 
 	message_box "Mail-in-a-Box Installation" \
 		"Hello and thanks for deploying a (Power) Mail-in-a-Box!
